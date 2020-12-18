ba055997f24c6108a7d706046d375392d4460ac1
Alex Mah <mahalex@umich.edu>
2020-12-18 11:46:48 -0500

    started header

--- header.php
+++ header.php
@@ -4,7 +4,33 @@
 	<title>"Zeeb Golf Driving Range"</title>
 </head>
 <body>
-	
+	<nav class="">
+		 <li class="nav-item">
+            <a class="nav-link hover-white" href="https://www.facebook.com/Zeeb-Golf-Driving-Range-207704295936470/"><b><img id="facebook_logo" src="facebook.png" alt="Zeeb Golf"></b>fac</a>
+        </l1>
+	</nav>
+	<nav class="navbar navbar-expand-lg navbar-custom bg-custom">
+            <a class="navbar-brand" href="index.php"><b><img id="zeeb_logo" src="zeeb_golf.jpg" alt="Zeeb Golf"></b></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link hover-white" href="account.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link hover-white" href="catalog.php">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link hover-white" href="updates.php">Photos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link hover-white" href="covid.php">COVID-19</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 </body>
 </html>
 
